8a2d26202667525f029ffba246ff84d3c3f7af06
Ronaldo Bueno <ronaldobueno2002@gmail.com>
2026-02-22 11:13:35 -0300

    Initial commit

---
+++ src/main/java/br/com/convoquei/backend/ConvoqueiBackendApplication.java
@@ -0,0 +1,13 @@
+package br.com.convoquei.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConvoqueiBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConvoqueiBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/convoquei/backend/ConvoqueiBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.convoquei.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConvoqueiBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
